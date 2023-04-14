852bb830a308f3b6b2a2669134bdfcb5bb9365d7
Andreas Hjalmarsson <Andreas Hjalmarsson@DESKTOP-9OO27JP>
2023-04-14 05:50:32 +0200

    te

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var http = require('http');
 fs = require('fs');
 
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+
+fs.readFile('./html/app.html', function (err, html))
+{
+    if (err) {
+        throw err;
+    }
+    http.createServer(function (req, res) {
+        console.log(`Just got a request at ${req.url}!`)
+        res.writeHeader(200, { 'Content-Type': 'text/html' });
+        res.write(html);
+        res.end();
+    }).listen(process.env.PORT || 3000);
+};
+
